eb5ccc8ef5ddf9f65ecf4b15eae3b47737b6da0d
Arend.von.Reinersdorff <arend@arendvr.com>
2013-04-28 15:09:34 +0200

    Reproduce problem with project.name

---
+++ src/test/java/com/example/outsidedirectorywithname/GreeterTest.java
@@ -0,0 +1,9 @@
+package com.example.outsidedirectorywithname;
+
+import com.example.outsidedirectorywithname.Greeter;
+
+public class GreeterTest {
+    public void testGreet() {
+        new Greeter().greet();
+    }
+}
